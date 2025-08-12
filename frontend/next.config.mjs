2f36be7734e3dd531ec699fc2cf66665b62b8e79
Anmol Khurana <anmolkhurana490@gmail.com>
2025-08-12 22:43:50 +0530

    next image optimization corrected

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     "images": {
-        "domains": [
-            process.env.NEXT_PUBLIC_IMAGE_DOMAIN || 'localhost',
-        ]
+        remotePatterns: [
+            {
+                protocol: process.env.NODE_ENV == 'production' ? 'https' : 'http',
+                hostname: process.env.NEXT_PUBLIC_IMAGE_DOMAIN || 'localhost',
+                pathname: '/api/uploads/**',
+            },
+        ],
     }
 };
 
